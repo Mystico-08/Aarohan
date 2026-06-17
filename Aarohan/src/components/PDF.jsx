6b4ded9decbe5e2dbe36253a68fba72f0d11efb7
Mystico_08 <gantavyamishra@gmail.com>
2026-06-18 01:05:18 +0530

    pls work pdf preview

--- Aarohan/src/components/PDF.jsx
+++ Aarohan/src/components/PDF.jsx
@@ -1,29 +1,26 @@
 import { useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
-import './PDF.css'
+import "./PDF.css";
 
 pdfjs.GlobalWorkerOptions.workerSrc =
   `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 const PDF = () => {
-  const [numPages, setNumPages] = useState();
+  const [numPages, setNumPages] = useState(0);
 
   return (
-    <div className="pdf-container">
+    <div className="pdf-wrapper">
       <Document
         file={`${import.meta.env.BASE_URL}Aarohan_SQP_9th.pdf`}
         onLoadSuccess={({ numPages }) => setNumPages(numPages)}
       >
-        {Array.from(
-          new Array(numPages),
-          (_, index) => (
-            <Page
-              key={index}
-              pageNumber={index + 1}
-              width={900}
-            />
-          )
-        )}
+        {Array.from({ length: numPages }, (_, i) => (
+          <Page
+            key={i}
+            pageNumber={i + 1}
+            width={window.innerWidth < 768 ? 320 : 800}
+          />
+        ))}
       </Document>
     </div>
   );
