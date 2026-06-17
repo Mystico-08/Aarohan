0e5e9fb85a43e17704403fcaa652681acee74ed5
Mystico_08 <gantavyamishra@gmail.com>
2026-06-18 00:37:32 +0530

    pdf preview

--- Aarohan/src/components/PDF.jsx
+++ Aarohan/src/components/PDF.jsx
@@ -4,9 +4,9 @@ const PDF = () => {
   return (
     <div className="container">
       <iframe
-        src="Aarohan_SQP_9th.pdf"
-        width="50%"
-        style={{ border: 'none', height:'clamp(400px,500px,700px)' }}
+        src={`${import.meta.env.BASE_URL}Aarohan_SQP_9th.pdf`}
+        width="60%"
+        style={{ border: 'none', height:'clamp(500px,80vh,800px)' }}
         allow='autoplay'
       />
     </div>
